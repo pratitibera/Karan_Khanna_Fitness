f709c1578eadcaad2ff5cfe628cf947334addca6
Pratiti Bera <remabimal0801@gmail.com>
2022-04-15 18:19:41 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// for animation of bar and cross in mobile view
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
